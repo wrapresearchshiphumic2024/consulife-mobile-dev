5b0e8b0786052eee6ccc8f4131d976a5197b7abd
Muhammad Faridz <faridzmuhamad679@gmail.com>
2024-11-27 15:22:53 +0700

    penambagan time slot

--- lib/app/modules/manage_schedule/controllers/manage_schedule_controller.dart
+++ lib/app/modules/manage_schedule/controllers/manage_schedule_controller.dart
@@ -4,7 +4,17 @@ class ManageScheduleController extends GetxController {
   //TODO: Implement ManageScheduleController
 
   final isOpenAppointment = false.obs;
+  final selectedDays = <String>[].obs;
+  final selectedTime = <Map<String, String>>[].obs;
+  void onSelectedDay(List<String> value) {
+    selectedDays.value = value;
+  }
+
   void onChanged(bool value) {
     isOpenAppointment.value = value;
+  }
+
+  void onSelectedTime(List<Map<String, String>> value) {
+    selectedTime.value = value;
   }
 }
--- lib/app/modules/manage_schedule/views/manage_schedule_view.dart
+++ lib/app/modules/manage_schedule/views/manage_schedule_view.dart
@@ -1,4 +1,6 @@
 import 'package:consulin_mobile_dev/app/constants/color.dart';
+import 'package:consulin_mobile_dev/widgets/ui/manage-day.dart';
+import 'package:consulin_mobile_dev/widgets/ui/manage-time.dart';
 import 'package:consulin_mobile_dev/widgets/ui/schedule_card.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,73 +14,104 @@ class ManageScheduleView extends GetView<ManageScheduleController> {
     final height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                child: Column(
                   children: [
-                    const Text("Schedule Overview",
-                        style: TextStyle(fontWeight: FontWeight.bold)),
-                    Row(
+                    _buildHeader(),
+                    const SizedBox(height: 5),
+                  ],
+                ),
+              ),
+              _buildScheduleCards(height),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    _buildChooseDaySection(),
+                    const SizedBox(height: 10),
+                    ManageDay(
+                      value: controller.selectedDays,
+                      onChange: controller.onSelectedDay,
+                      disabled: true,
+                    ),
+                    const SizedBox(height: 20),
+                    const Row(
                       children: [
-                        const Text("Schedule Active"),
-                        Obx(() {
-                          return Switch(
-                            focusColor: primaryColor,
-                            activeColor: primaryColor,
-                            inactiveThumbColor: primaryColor.withOpacity(0.5),
-                            value: controller.isOpenAppointment.value,
-                            onChanged: (value) {
-                              controller.onChanged(value);
-                            },
-                          );
-                        }),
+                        Text("Set Available Time Slot",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        SizedBox(width: 5),
+                        Text("(can multiple select)"),
                       ],
                     ),
+                    ManageTime(
+                      onChange: controller.onSelectedTime,
+                      value: controller.selectedTime,
+                      disabled: true,
+                    ),
                   ],
                 ),
-                const SizedBox(
-                    height: 5), // Menambahkan jarak antara header dan list
-                SizedBox(
-                  height: height / 6, // Menentukan tinggi untuk ListView
-                  child: ListView.separated(
-                    itemCount: 5,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return const ScheduleCard(
-                          title: "Monday", time: "10:00 - 11:00");
-                    },
-                    separatorBuilder: (context, index) {
-                      return const SizedBox(
-                        width: 15,
-                      );
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Row(
-                  children: [
-                    const Text(
-                      "Choose Day",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      width: 5,
-                    ),
-                    Text("(can multiple select)"),
-                  ],
-                )
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text("Schedule Overview",
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        Row(
+          children: [
+            const Text("Schedule Active"),
+            Obx(() {
+              return Switch(
+                focusColor: primaryColor,
+                activeColor: primaryColor,
+                inactiveThumbColor: primaryColor.withOpacity(0.5),
+                value: controller.isOpenAppointment.value,
+                onChanged: controller.onChanged,
+              );
+            }),
+          ],
+        ),
+      ],
+    );
+  }
+
+  SizedBox _buildScheduleCards(double height) {
+    return SizedBox(
+      height: height / 6,
+      child: ListView.separated(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        itemCount: 5,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return const ScheduleCard(title: "Monday", time: "10:00 - 11:00");
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(width: 15);
+        },
+      ),
+    );
+  }
+
+  Row _buildChooseDaySection() {
+    return const Row(
+      children: [
+        Text("Choose Day", style: TextStyle(fontWeight: FontWeight.bold)),
+        SizedBox(width: 5),
+        Text("(can multiple select)"),
+      ],
     );
   }
 }
